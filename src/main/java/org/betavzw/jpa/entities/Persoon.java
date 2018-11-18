3fec19eb569e052d037b09ac83895a2addf33ea7
Jef De Smedt <Jef.DeSmedt@betavzw.org>
2018-11-18 13:36:39 +0100

    Entity Persoon toevoegen

---
+++ src/main/java/org/betavzw/jpa/entities/Persoon.java
@@ -0,0 +1,38 @@
+package org.betavzw.jpa.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Persoon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String naam;
+
+    public Persoon() {
+    }
+
+    public Persoon(Long id, String naam) {
+        this.id = id;
+        this.naam = naam;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+}
