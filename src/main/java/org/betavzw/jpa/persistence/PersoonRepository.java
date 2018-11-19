ea0bda3df8621141183efc9fbd37433c78c8d15c
Jef De Smedt <Jef.DeSmedt@betavzw.org>
2018-11-19 06:20:03 +0100

    deletePersoon geeft boolean terug

--- src/main/java/org/betavzw/jpa/persistence/PersoonRepository.java
+++ src/main/java/org/betavzw/jpa/persistence/PersoonRepository.java
@@ -6,9 +6,8 @@ import java.util.Collection;
 
 public interface PersoonRepository {
     Persoon addPersoon(String naam);
-    void deletePersoon(Persoon persoon) ;
+    boolean deletePersoon(Persoon persoon) ;
     Persoon getPersoon(long id);
     void updatePersoon(Persoon persoon);
     Collection<Persoon> getPersonen();
-
 }
