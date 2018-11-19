0f1d79761f629714b4b18057795a6e2654c741b8
Jef De Smedt <Jef.DeSmedt@betavzw.org>
2018-11-19 06:58:49 +0100

    client gemaakt

--- src/main/java/org/betavzw/jpa/App.java
+++ src/main/java/org/betavzw/jpa/App.java
@@ -1,5 +1,13 @@
 package org.betavzw.jpa;
 
+import org.betavzw.jpa.entities.Persoon;
+import org.betavzw.jpa.persistence.NotFoundException;
+import org.betavzw.jpa.persistence.PersoonRepository;
+import org.betavzw.jpa.persistence.memory.MemoryPersoonRepository;
+
+import java.util.Collection;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,38 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        PersoonRepository repository = new MemoryPersoonRepository();
+        for (int i = 0; i< 3;i++){
+            System.out.print("Geef naam: ");
+            String naam = scanner.nextLine();
+            Persoon p = repository.addPersoon(naam);
+            System.out.printf("Persoon %s toegevoegd met id %d%n", p.getNaam(), p.getId());
+        }
+        Collection<Persoon> personen = repository.getPersonen();
+        for(Persoon p: personen) {
+            System.out.printf("%d: %s%n", p.getId(), p.getNaam());
+        }
+        System.out.print("Geef het nummer van de persoon die u wil aanpassen: ");
+        long id = Integer.parseInt(scanner.nextLine());
+        System.out.print("Geef de nieuwe naam: ");
+        String naam = scanner.nextLine();
+        Persoon nieuwePersoon = new Persoon(id, naam);
+        try {
+            repository.updatePersoon(nieuwePersoon);
+        }catch(NotFoundException nf){
+            System.out.println(nf.getMessage());
+        }
+        personen = repository.getPersonen();
+        for(Persoon p: personen) {
+            System.out.printf("%d: %s%n", p.getId(), p.getNaam());
+        }
+        System.out.println("Laatste persoon verwijderen:");
+        repository.deletePersoon(nieuwePersoon);
+        personen = repository.getPersonen();
+        for(Persoon p: personen) {
+            System.out.printf("%d: %s%n", p.getId(), p.getNaam());
+        }
     }
+}
 }
